85df99b93153b1ad236cb85f6e2fc352209c336b
BiswasJishnu <biswasjishnu24@gmail.com>
2021-04-20 09:23:09 +0530

    imported local module and removed unnesscary functions

--- sysfetch_curses.py
+++ sysfetch_curses.py
@@ -1,120 +1,24 @@
 #!/usr/bin/python3
 import curses
 import time
-import psutil
 import os
-
-#CPU FUNCTIONS
-
-def get_cpu_usage_pct():
-    """
-    Obtains the system's average CPU load as measured over a period of 500 milliseconds.
-    :returns: System CPU load as a percentage.
-    :rtype: float
-    """
-    return psutil.cpu_percent(interval=0.5)
-
-def get_cpu_frequency():
-    """
-    Obtains the real-time value of the current CPU frequency.
-    :returns: Current CPU frequency in MHz.
-    :rtype: int
-    """
-    return int(psutil.cpu_freq().current)
-
-def get_cpu_temp():
-    """
-    Obtains the current value of the CPU temperature.
-    :returns: Current value of the CPU temperature if successful, zero value otherwise.
-    :rtype: float
-    """
-    # Initialize the result.
-    result = 0.0
-    # The first line in this file holds the CPU temperature as an integer times 1000.
-    # Read the first line and remove the newline character at the end of the string.
-    if os.path.isfile('/sys/class/thermal/thermal_zone0/temp'):
-        with open('/sys/class/thermal/thermal_zone0/temp') as f:
-            line = f.readline().strip()
-        # Test if the string is an integer as expected.
-        if line.isdigit():
-            # Convert the string with the CPU temperature to a float in degrees Celsius.
-            result = float(line) / 1000
-    # Give the result back to the caller.
-    return result
-
-
-
-
-
-#RAM FUNCTIONS
-
-def get_ram_total():
-    """
-    Obtains the total amount of RAM in bytes available to the system.
-    :returns: Total system RAM in bytes.
-    :rtype: int
-    """
-    return int(psutil.virtual_memory().total)
-
-def get_ram_usage_pct():
-    """
-    Obtains the system's current RAM usage.
-    :returns: System RAM usage as a percentage.
-    :rtype: float
-    """
-    return psutil.virtual_memory().percent
-
-def get_ram_usage():
-    """
-    Obtains the absolute number of RAM bytes currently in use by the system.
-    :returns: System RAM usage in bytes.
-    :rtype: int
-    """
-    return int(psutil.virtual_memory().total - psutil.virtual_memory().available)
-
-#SWAP FUNCTIONS
-
-def get_swap_usage():
-    """
-    Obtains the absolute number of Swap bytes currently in use by the system.
-    :returns: System Swap usage in bytes.
-    :rtype: int
-    """
-    return int(psutil.swap_memory().used)
-
-def get_swap_total():
-    """
-    Obtains the total amount of Swap in bytes available to the system.
-    :returns: Total system Swap in bytes.
-    :rtype: int
-    """
-    return int(psutil.swap_memory().total)
-
-def get_swap_usage_pct():
-    """
-    Obtains the system's current Swap usage.
-    :returns: System Swap usage as a percentage.
-    :rtype: float
-    """
-    return psutil.swap_memory().percent
-
-
+import sysfetch_functions
 
 #OUTPUT
 
 def output(stdscr):
 
-    cpu_freqz = ' CPU frequency : {} MHz'.format(get_cpu_frequency())
-    cpu_useage_pct = ' CPU usage : {}%'.format(get_cpu_usage_pct())
-    cpu_temp = ' CPU temperature : {}°C'.format(get_cpu_temp())
+    cpu_freqz = ' CPU frequency : {} MHz'.format(sysfetch_functions.get_cpu_frequency())
+    cpu_useage_pct = ' CPU usage : {}%'.format(sysfetch_functions.get_cpu_usage_pct())
+    cpu_temp = ' CPU temperature : {}°C'.format(sysfetch_functions.get_cpu_temp())
 
-    ram_used = ' System RAM used : {} MB'.format(int(get_ram_usage() / 1024 / 1024))
-    ram_total = ' Total available RAM : {} MB'.format(int(get_ram_total() / 1024 / 1024))
-    ram_useage_pct=' RAM usage : {}%'.format(get_ram_usage_pct())
+    ram_used = ' System RAM used : {} MB'.format(int(sysfetch_functions.get_ram_usage() / 1024 / 1024))
+    ram_total = ' Total available RAM : {} MB'.format(int(sysfetch_functions.get_ram_total() / 1024 / 1024))
+    ram_useage_pct=' RAM usage : {}%'.format(sysfetch_functions.get_ram_usage_pct())
 
-    swap_used = ' SWAP used : {} MB'.format(int(get_swap_usage() / 1024 / 1024))
-    swap_total = ' Total available SWAP : {} MB'.format(int(get_swap_total() / 1024 / 1024))
-    swap_useage_pct = ' Swap usage : {}%'.format(get_swap_usage_pct())
+    swap_used = ' SWAP used : {} MB'.format(int(sysfetch_functions.get_swap_usage() / 1024 / 1024))
+    swap_total = ' Total available SWAP : {} MB'.format(int(sysfetch_functions.get_swap_total() / 1024 / 1024))
+    swap_useage_pct = ' Swap usage : {}%'.format(sysfetch_functions.get_swap_usage_pct())
 
     stdscr.addstr(0,1,"CPU Status")
     stdscr.addstr(1,1,cpu_freqz)
